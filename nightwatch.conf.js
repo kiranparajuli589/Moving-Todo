e4b245f53146fb9ce4cd9a9d350c9c47ee041dd1
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-01-02 16:58:27 +0545

    test setup for firefox

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,11 +1,33 @@
+const chromeDriver = require('chromedriver')
+const geckoDriver = require('geckodriver')
+
 module.exports = {
     src_folders: ['test'],
     page_objects_path: './test/acceptance/pageObjects',
+    // for chrome driver
+    webdriver : {
+        start_process: true,
+        server_path: chromeDriver.path,
+        port: 9515
+    },
+    // for gecko driver
+    // webdriver: {
+    //     start_process: false, // set true if you are not running selenium manually
+    //     port: 4444,
+    //     server_path: geckoDriver.path,
+    //     cli_args: [
+    //         "--log", "debug"
+    //         // very verbose geckodriver logs
+    //         // '-vv'
+    //     ]
+    // },
     test_settings: {
         default: {
             selenium_host: '127.0.0.1',
-            launch_url: process.env.SERVER_HOST || 'http://127.0.0.1:8000',
+            selenium_port: 4444,
+            launch_url: 'http://127.0.0.1:8000',
             globals: {},
+            // for chrome
             desiredCapabilities: {
                 browserName: 'chrome',
                 javascriptEnabled: true,
@@ -14,6 +36,20 @@ module.exports = {
                     w3c: false
                 }
             }
+            // for mozilla firefox
+            // desiredCapabilities: {
+            //     browserName: 'firefox',
+            //     alwaysMatch: {
+            //         // Enable this if you encounter unexpected SSL certificate errors in Firefox
+            //         acceptInsecureCerts: true,
+            //         // 'moz:firefoxOptions': {
+            //         //     args: [
+            //         //         // '-headless',
+            //         //         '-verbose'
+            //         //     ],
+            //         // }
+            //     }
+            // }
         }
     }
-};
+}
