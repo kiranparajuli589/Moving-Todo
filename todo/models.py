66a2698b998b27826fe15569def71d41b575fcc4
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-04-13 00:58:07 +0545

    UI updates

--- todo/models.py
+++ todo/models.py
@@ -9,5 +9,5 @@ NOW = KTM.localize(datetime.now())
 class Todo(models.Model):
     element_title = models.CharField(max_length=255, null=False,  unique=True)
     content = models.TextField()
-    position = models.IntegerField(unique=True)
+    position = models.IntegerField()
     date_created = models.DateTimeField(auto_now_add=NOW)
